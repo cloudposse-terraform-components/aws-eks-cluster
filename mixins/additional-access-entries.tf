d3ac624d2be400dbdc2ca49b5212416e140f9b1d
Benjamin Smith <ben.smith.developer@gmail.com>
2026-03-13 15:20:50 -0700

    mixins fixes

---
+++ mixins/additional-access-entries.tf
@@ -0,0 +1,9 @@
+locals {
+  # If you have custom access entries and policies, create a file called `additional-access-entries_override.tf`
+  # and in that file override any of the following declarations as needed.
+  #
+  # For example, we use this to add a policy for the GitHub Actions OIDC role
+  overridable_access_entry  = []
+  overridable_access_map    = {}
+  overridable_access_policy = []
+}
